c0a1998efe5766b440eb73078f7469c0cd744a5d
Mohammad Foroughi <foroughi1380@gmail.com>
2023-03-25 22:55:26 +0330

    setting page complete

--- src/js/setting.js
+++ src/js/setting.js
@@ -1,35 +1,83 @@
-function main(){
-    window.store = Alpine.store
+var context = chrome;
 
-    store('host' , '')
-    store('headers', {
-        data : [],
-        get(key = null){
-            if (key){
-                for(let a in this.data){
-                    if (a.key == key){
-                        return a;
-                    }
-                }
-            }
-            return this.data;
-        },
-        append(key , value){
-            console.log("value added")
-            this.data.push({key : key , value : value});
-        },
-        set(index , key, value){
-            this.data[index] = {key : key , value : value};
-        },
-        delete(index){
-            console.log(index)
-            this.data.splice(index , 1);
-            console.log(this.data)
-        }
-    })
+var HostAddress = document.querySelector("#hostAddress")
+var HeadersContainer = document.querySelector("#headers_container")
+var rowTemplate = document.querySelector('#header_row_template')
 
-    Alpine.start();
-    console.log("Alpine started")
+var Headers = []
+
+function main() {
+    addEvents()
+    loadData();
+}
+
+function addEvents(){
+    document.querySelector("#btn_save_host").addEventListener('click' , saveHost)
+    document.querySelector("#btn_add_header_row").addEventListener('click' , addHeaderRow)
+    document.querySelector("#btn_save_headers").addEventListener('click' , saveHeaders)
+}
+
+function saveHeaders(){
+    context.storage.sync.set({
+        'headers': Headers,
+    } , function () {
+        window.alert('Headers saved')
+    });
+}
+
+function loadData(){
+    context.storage.sync.get({'host' : "http://example.com/api/headers.php" , 'headers' : []}, function(items) {
+        HostAddress.value = items.host;
+        Headers = items.headers;
+        printHeaders()
+    });
+}
+
+
+function addHeaderRow(e = null, key="" , value=""){
+    Headers.push({key:key , value:value})
+    printHeaders()
+}
+
+function removeHeaderRow(index){
+    Headers.splice(index , 1)
+    printHeaders()
+}
+
+function printHeaders(){
+    HeadersContainer.innerHTML = '';
+
+    for (let i = 0; i < Headers.length; i++) {
+        let row = rowTemplate.content.cloneNode(true);
+        row.querySelector(".key-input-template").value = Headers[i].key
+        row.querySelector(".value-input-template").value = Headers[i].value
+        row.querySelector(".btn-remove-input-template").addEventListener('click' , () => removeHeaderRow(i));
+        row.querySelector(".value-input-template").addEventListener('input' , (e) => {
+            Headers[i].value = e.target.value
+        });
+        row.querySelector(".key-input-template").addEventListener('input' , (e) => {
+            Headers[i].key = e.target.value
+        });
+        HeadersContainer.appendChild(row);
+    }
+
+}
+
+function saveHost(){
+    let address = HostAddress.value;
+
+    let expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+    let regex = new RegExp(expression);
+
+    if (address.match(regex)) {
+        context.storage.sync.set({
+            'host': address,
+        } , function () {
+            window.alert('address saved')
+        });
+    }else{
+        window.alert('address is invalid')
+    }
 }
 
 main();
